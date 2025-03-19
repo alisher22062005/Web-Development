397af1906d3c8e9341d6948cecafd2651e40eeb5
alisher22062005 <156660415+alisher22062005@users.noreply.github.com>
2025-03-19 20:28:41 +0500

    Delete CodingBat directory

--- CodingBat/List-2/centered_average.py
+++
@@ -1,8 +0,0 @@
-def centered_average(nums):
-    # Create a copy so we don't modify the original list
-    nums_copy = nums[:]
-    # Remove one instance of the smallest and one instance of the largest value.
-    nums_copy.remove(min(nums_copy))
-    nums_copy.remove(max(nums_copy))
-    # Use integer division to compute the average of the remaining numbers.
-    return sum(nums_copy) // len(nums_copy)
